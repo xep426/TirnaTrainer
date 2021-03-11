dc154b37c1d4f9a12ad4a69524c9833d06d02a2f
xep426 <xep426@gmail.com>
2021-03-11 23:45:08 +0100

    Initial commit

---
+++ app/src/main/java/com/gmail/xep426/tirnatrainer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.xep426.tirnatrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
